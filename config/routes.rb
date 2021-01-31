5a3166bd99386378ba55d8f51dd7c553d389ef63
Rafael H. Osorio <rafa.osorioa@gmail.com>
2021-01-31 14:29:56 -0500

    Changed the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/basics'
-  get 'static_pages/ruby'
-  get 'static_pages/git'
+  get '/basics', to: 'static_pages#basics', as: 'basics'
+  get '/ruby', to: 'static_pages#ruby', as: 'ruby'
+  get '/git', to: 'static_pages#git', as: 'git'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "static_pages#home"
+  root 'static_pages#home'
 end
